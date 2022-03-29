51efb4b153ec13654a40184dab0790e0146dd042
Nils Steinger <git@n-st.de>
2022-03-29 02:33:39 +0200

    Document postprocessing steps

--- axi2xsd.sh
+++ axi2xsd.sh
@@ -20,7 +20,9 @@ then
     exit 1
 fi
 
-# To cut off the header and footer text, we need to know their areas.
+# During extraction, we cut off the page header and footer, so their text
+# doesn't mix with the XSD data.
+# For this, we need to know the heights of the header and footer areas.
 # You can use 'pdftotext -bbox-layout' to get a detailed XML representation of
 # the page geometry, and look up the header and footer blocks there.
 #PAGEHEIGHT=842 # not needed, we use FOOTER_YMIN for our calculation
@@ -30,6 +32,13 @@ FOOTER_YMIN=786
 
 FIRST_PAGE=392
 LAST_PAGE=506
+
+# The XSD data from the PDF can contain some errors that need to be smoothed over:
+# - attribute values may erroneously contain whitespace, e.g.
+#   <attribute name="host" type="string" use=" optional "/>
+# - " may be rendered as “ (Unicode Left Double Quotation Mark)
+# - First and last page may contain extra text (e.g. heading), so extract only
+#   from <?xml to </schema>.
 
 pdftotext -layout -nopgbrk -f "$FIRST_PAGE" -l "$LAST_PAGE" -x 0 -y "$HEADER_YMAX" -W "$PAGEWIDTH" -H $((FOOTER_YMIN - HEADER_YMAX)) "$PDFPATH" - \
     | sed 's@\([[:alnum:]]\+\)="[[:space:]]\+\([[:alnum:]]\+\)[[:space:]]\+"@\1="\2"@g' \
